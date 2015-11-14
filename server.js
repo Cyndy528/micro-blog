4f6b315cf28604274fd0debc07dddd042b8bd5e4
Cynthia Salinas <csalinas123@gmail.com>
2015-11-14 14:42:44 -0800

    server route to get all pies

--- server.js
+++ server.js
@@ -4,21 +4,50 @@ var express = require('express'),
 	bodyParser = require('body-parser'); 
 
 //configure body-parser (for form data)
-app.user(bodyParser.urlencoded({ extended: true})); 
+app.use(bodyParser.urlencoded({ extended: true})); 
 
-//use public folder for static files
-app.use(express.static(_dirname + '/public')); 
+// use public folder for static files
+app.use(express.static(__dirname + '/public'));
 
 //set hbs as server view engine
 app.set('view engine', 'hbs'); 
 
 // HOMEPAGE ROUTE
 app.get('/', function(req,res) {
-	res.send('hello world');
+	res.render('index');
 });
+
+// API Routes 
+// Test data
+	var allFruitPies = [
+		{fruitPie: 'Bosenberry'}, 
+		{fruitPie: 'Apple'}, 
+		{fruitPie: 'Banana Cream Pie'}
+	];
+
+	//array of test data
+	var allSavoryPies = [
+		{savoryPie: 'Chicken Pot Pie'},
+		{savoryPie: 'Spinach and Cheddar Tart'}, 
+		{savoryPie: "Shepherd's Pie"}
+	];
+
+	//array of test data
+	var allInternationalPies = [
+		{internationalPie: 'Chicken Pot Pie'},
+		{internationalPie: 'Aloo Pie'}, 
+		{internationalPie: 'Linzertorte'}
+	];
+
+app.get('/api/FruitPies', function (req, res) {
+  FruitPies.find(function (err, allFruitPies) {
+    res.json({ FruitPies: allFruitPies });
+  });
+});
+
 
 // start server on localhost:3000
 app.listen(3000, function() {
-	console.log('server stared'); 
+  console.log('server started');
+});
 
-}); 
